adc7b7250cc36b49f1c357cf7062f539298dea13
Monteiroman <monteirotiagom90@gmail.com>
2018-10-26 10:55:13 -0300

    Ussing modules

--- FFT_Magnitude/core.py
+++ FFT_Magnitude/core.py
@@ -7,140 +7,18 @@ Created on Tue Oct 22 10:35:23 2018
 """
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp, QWidget
-from PyQt5.QtWidgets import QLabel, QLineEdit, QTextEdit, QGridLayout
-from PyQt5.QtWidgets import QApplication, QPushButton, QHBoxLayout, QFrame, QVBoxLayout
 import matplotlib.pyplot as plt
 import numpy as np
 import math as mat
-
 import time
 
 # Traemos la libreria VISA
 import pyvisa as visa
 
 # Agreamos el path de las librerias
-#import sys
 sys.path.insert(0, 'Libreria')
 # Traemos la clase base que implmenta las funciones de VISA
 from instrument import Instrument
-
-
-
-class ConneTC_GUI(QMainWindow):
-
-    instrumentList = []
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-
-    def initUI(self):
-        self.ex = HorizontalBox()
-        self.setCentralWidget(self.ex)
-
-        self.statusBar()
-
-        ConnectBtn = QPushButton("Conectar", self.ex.rightFrame)
-        ConnectBtn.resize (80,30)
-        ConnectBtn.clicked.connect(self.connectButtonClicked)
-        self.ex.rightGridLayout.addWidget(ConnectBtn, 1, 0)
-
-        StartBtn = QPushButton("Comenzar", self.ex.rightFrame)
-        StartBtn.resize (80,30)
-        StartBtn.clicked.connect(self.startButtonClicked)
-        self.ex.rightGridLayout.addWidget(StartBtn, 2, 0)
-
-        ExitBtn = QPushButton("Salir", self.ex.rightFrame)
-        ExitBtn.resize (80,30)
-        ExitBtn.clicked.connect(qApp.quit)
-        self.ex.rightGridLayout.addWidget(ExitBtn, 3, 0)
-
-        self.command_answer = QLabel('FFT Magnitude')
-        self.ex.leftGridLayout.addWidget(self.command_answer, 2, 1)
-
-        self.setGeometry(300, 300, 800, 200)
-        self.setWindowTitle('Command Test')
-        self.show()
-
-
-    def connectButtonClicked (self):
-        s, self.instrumentList = SearchInstrument(self)
-        if self.instrumentList:
-            self.instrument = SelectInstrument(self.instrumentList)
-
-        self.statusBar().showMessage(s)
-
-
-    def startButtonClicked (self):
-        if self.instrumentList:
-            StartMeasure(self.instrument)
-        else:
-            self.statusBar().showMessage("Primero debe dar \"Conectar\"")
-
-
-
-
-
-class HorizontalBox(QWidget):
-
-
-    def __init__(self):
-        super().__init__()
-        self.initBox()
-
-
-    def initBox(self):
-
-        self.principalLayout = QHBoxLayout(self)
-        #___________Left Frame___________
-        self.leftFrame = QFrame(self)
-        self.leftFrame.setFrameShape(QFrame.StyledPanel)
-        self.leftFrame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout = QVBoxLayout(self.leftFrame)
-        self.leftGridLayout = QGridLayout()
-        self.verticalLayout.addLayout(self.leftGridLayout)
-        self.principalLayout.addWidget(self.leftFrame)
-        #___________Right Frame___________
-        self.rightFrame = QFrame(self)
-        self.rightFrame.setFrameShape(QFrame.StyledPanel)
-        self.rightFrame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout = QVBoxLayout(self.rightFrame)
-        self.rightGridLayout = QGridLayout()
-        self.verticalLayout.addLayout(self.rightGridLayout)
-        self.principalLayout.addWidget(self.rightFrame)
-
-
-
-
-def SearchInstrument (self):
-    self.instrumentList = []
-    # Pedimos la lista de instrumentos
-    rm=visa.ResourceManager('@py')
-    #print(rm.list_resources('?*'))
-    #print(rm.list_resources())
-
-    if rm.list_resources():
-        s = ("")
-        for resource_obj in rm.list_resources():
-	    # Abrimos un instrumento
-            instrument_handler = rm.open_resource(resource_obj)
-	    # Implementamos la clase instrumento base
-            instrumento = Instrument(instrument_handler)
-            self.instrumentList.append(instrumento)
-	    # Imprimimos el ID el instrumento
-            s = s + "\t" + instrumento.get_ID() + "\n"
-        self.auxString = "Instrumentos conectados: " + s
-        return self.auxString, self.instrumentList
-
-    else:
-        self.auxString = "No hay dispositivos para conectarse"
-        return self.auxString, self.instrumentList
-
-
-def SelectInstrument (instrumentList):
-    return instrumentList[0]
 
 
 def StartMeasure(instrument):
@@ -223,11 +101,11 @@ def AnalyzeFile():
 
     return 1
 
-
+# This main is left for debugging the measure file
 if __name__ == '__main__':
 
     AnalyzeFile()
 
-    # app = QApplication(sys.argv)
-    # ex = ConneTC_GUI()
-    # sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = ConneTC_GUI()
+    sys.exit(app.exec_())
